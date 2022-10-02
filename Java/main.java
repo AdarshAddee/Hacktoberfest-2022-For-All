bf2b9790c4bffce8c70f67e731fcab48a01925af
Adarsh Addee <59617563+AdarshAddee@users.noreply.github.com>
2022-10-02 16:11:02 +0530

    Create main.java

---
+++ Java/main.java
@@ -0,0 +1,11 @@
+// Github username: AdarshAddee
+// Aim: Greeting everyone
+// Date: 02 September 2022
+
+class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+
