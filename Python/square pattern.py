93d305e5669e8af9dfd1b26d33b92cbd94696647
Adarsh Code <112389532+AdrshCode@users.noreply.github.com>
2022-10-02 16:15:33 +0530

    Create square pattern.py

---
+++ Python/square pattern.py
@@ -0,0 +1,13 @@
+# Github username: AdrshCode
+# Aim: Square Pattern in Python
+# Date: 02 October 2022
+
+print("Square Star Pattern") 
+
+for i in range(4):
+    for i in range(side):
+        print('*', end = '  ')
+    print()
+    
+    
+    
