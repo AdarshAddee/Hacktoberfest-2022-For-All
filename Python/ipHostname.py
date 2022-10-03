2253fc8114d4588f65c7c73bcef993b01526c82d
Mikail Asada <62200797+Asadaaaaa@users.noreply.github.com>
2022-10-03 16:19:54 +0700

    Add files via upload

---
+++ Python/ipHostname.py
@@ -0,0 +1,14 @@
+# Github username: Asadaaaaa
+# Aim: 
+# Date: 10/03/22
+
+# start coding
+
+
+import socket
+
+host = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+print(f"Hostname: {host}")
+print(f"IP Address: {ip}")
